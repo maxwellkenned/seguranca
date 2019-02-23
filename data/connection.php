b3e1d8344dffc8b23ea0e12068ff5476a99edb1d
Vanjhony Cosmo <jhonycosmo@gmail.com>
2019-02-23 14:18:13 -0300

    Criando classe de conexao

--- data/connection.php
+++ data/connection.php
@@ -1,8 +1,29 @@
 <?php
+namespace data;
+
+class Connection {
+    static var $pdo;
+
+    static public getPDO(){
+
+        if $pdo==null{
+
+            $host="localhost";
+            $db_name="db_seguranca_web";
+            $db_user="root";
+            $db_password="";
+            $pdo = new PDO("mysql:host=". $host .";dbname=".$db_name, $db_user, $db_password); 
+
+        }
+
+        return $pdo;
+    }
+
+}
 //PDO Conection
-$host="localhost";
-$db_name="db_seguranca_web";
-$db_user="root";
-$db_password="";
-$pdo = new PDO("mysql:host=". $host .";dbname=".$db_name, $db_user, $db_password);
+//$host="localhost";
+//$db_name="db_seguranca_web";
+//$db_user="root";
+//$db_password="";
+//$pdo = new PDO("mysql:host=". $host .";dbname=".$db_name, $db_user, $db_password);
 ?>
